7922bf281238ab8a38f3471940fe76f2a002357f
CourtneyODonnell <courtney.odonnell.cormier@gmail.com>
2022-09-01 20:58:31 -0300

    recieve the props for a player

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,21 +1,17 @@
 import React from "react"; //optional
 
 //add a return statement that will return the Player HTML from the project overview reading
-function Player() {
-  return (
-    <article className="Player">
-      <h1>
-        John <span>Secret</span> Doe
-      </h1>
-      {/* to be shown when there are no wins */}
-      <h2 className="zero">Currently with no wins :( </h2>
-      {/* to be shown when there is 1 win */}
-      <h2>Currently at 1 win</h2>
-      {/* to be shown when there are multiple wins */}
-      <h2>Currently at 1+ wins</h2>
-    </article>
-  );
-}
+function Player(props) {
+
+    return (
+      <article className="Player">
+        <h1>{props.firstName} <span>{props.gamerTag}</span> {props.lastName}</h1>
+        {/* To be shown when there is no wins */}<h2 className="zero">Currently with no wins </h2>:(
+        {/* To be shown when there is 1 win */}<h2>Currently at 1 win</h2>
+        {/* To be shown when there is more than one win */}<h2>Currently at {props.wins} wins</h2>
+      </article>
+    );
+  }
 
 //after function, write statement to export it
 export default Player;
--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -5,15 +5,18 @@ import playerData from '../data/playerData';
 import { preparePlayerData, addWinsToPlayers } from '../helpers/playerHelpers';
 import Player from "./Player";
 
-export default function PlayerList(props) {
-	const playerDataArray = preparePlayerData(playerData);
-	const parsedPlayerData = addWinsToPlayers(playerDataArray, matchData);
-	console.log(playerDataArray, parsedPlayerData);
+function PlayerList(props) {
+
+  const playerDataArray = preparePlayerData(playerData);
+  const parsedPlayerData = addWinsToPlayers(playerDataArray, matchData);
+  const onePlayer = parsedPlayerData[0];
 
   return (
     <section className="PlayerList">
       <h1>Current participating players</h1>
-      <Player />
+      <Player gamerTag={onePlayer.gamerTag} firstName={onePlayer.firstName} lastName={onePlayer.lastName} wins={onePlayer.wins} />
     </section>
   );
-};
+}
+
+export default PlayerList;
