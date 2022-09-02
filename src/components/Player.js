fdb6576c751cd0f8d800dff4f54e948830c03f22
CourtneyODonnell <courtney.odonnell.cormier@gmail.com>
2022-09-01 21:10:42 -0300

    conditional rendering

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,17 +1,16 @@
 import React from "react"; //optional
 
 //add a return statement that will return the Player HTML from the project overview reading
-function Player(props) {
-
-    return (
-      <article className="Player">
-        <h1>{props.firstName} <span>{props.gamerTag}</span> {props.lastName}</h1>
-        {/* To be shown when there is no wins */}<h2 className="zero">Currently with no wins </h2>:(
-        {/* To be shown when there is 1 win */}<h2>Currently at 1 win</h2>
-        {/* To be shown when there is more than one win */}<h2>Currently at {props.wins} wins</h2>
-      </article>
-    );
-  }
+function Player (props) {
+  return (
+    <article className="Player">
+      <h1>{props.firstName} <span>{props.gamerTag}</span> {props.lastName}</h1>
+      {props.wins === 0 && <h2 className="zero">Currently with no wins</h2>}
+      {props.wins === 1 && <h2>Currently at {props.wins} win</h2>}
+      {props.wins > 1 && <h2>Currently at {props.wins} wins</h2>}
+    </article>
+  );
+};
 
 //after function, write statement to export it
 export default Player;
